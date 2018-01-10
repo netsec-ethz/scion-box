b1b493074e5ab2b4cab3ad2382a610eb18b90e2e
jonghoonkwon <jonghoonkwon@gmail.com>
2018-01-10 18:59:11 +0100

    Fixed path issues in update_gen.py

--- scionLab.sh
+++ scionLab.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# This file is located in $SCIONPATH and called periodically by cron or systemd
+# This file is located in $SCIONBOX and called periodically by cron or systemd
 
-export PYTHONPATH=../scion/python/:sub/util/:.
+export PYTHONPATH=../scion/.:../scion/python/:sub/util/:.
 
 python3 ./update_gen.py
