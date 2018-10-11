1230a9b5f2189e5e27ce28e010abc6b2db6a28a2
François Wirz <wirzf@inf.ethz.ch>
2018-10-11 18:54:42 +0200

    Parse updateGen.sh parameters

--- updateGen.sh
+++ updateGen.sh
@@ -17,9 +17,27 @@ fi
 IP_ADDR=$(hostname -I | awk '{print $1}')
 URL="https://www.scionlab.org"
 
+# Parse named arguments string into array
+IFS=$'\n' ARGS=($(echo " $*" | awk '{split($0, out, " --"); for(e in out) {print out[e];}}'))
+IFS=" "
+for ARG in "${ARGS[@]}"
+do
+    # Split key=value
+    KEY=$(echo $ARG | cut --fields=1 --only-delimited --delimiter=" ")
+    VALUE=$(echo $ARG | cut --fields=2 --only-delimited --delimiter=" ")
+    case "$KEY" in
+        url)          URL=${VALUE} ;;
+        updateAS)     IAS=($(echo ${VALUE}));; # IAS is an array
+        accountId)    ACC_ID=${VALUE} ;;
+        secret)       ACC_PW=${VALUE} ;;
+        address)      IP_ADDR=${VALUE} ;;
+        *)
+    esac
+done
+
 export PYTHONPATH=$SC:$SC/python:$BASE/sub/util/:$BASE
-while read -r IA; do
+for IA in "${IAS[@]}"; do
     echo "Updating AP for IA: $IA ..."
-    python3 $BASE/update_gen.py --url "$URL" --updateAS "$IA" --accountId "$ACC_ID" --secret "$ACC_PW" --address "$IP_ADDR" --fullsync $*
-done <<< "$IAS"
+    python3 $BASE/update_gen.py --url "$URL" --updateAS "$IA" --accountId "$ACC_ID" --secret "$ACC_PW" --address "$IP_ADDR" --fullsync
+done
 echo "Done updating AP."
